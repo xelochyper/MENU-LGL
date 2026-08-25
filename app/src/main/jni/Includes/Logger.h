f206d946d5b8682df62650a56e5c7fb3f75fa01c
xєꙆo <xelo.chyper@gmail.com>
2026-08-25 20:18:30 +0700

    Add files via upload

--- app/src/main/jni/Includes/Logger.h
+++ app/src/main/jni/Includes/Logger.h
@@ -1,28 +1,21 @@
-//
-//  Logger.h
-//
-//  Created by MJ (Ruit) on 1/1/19.
-//
+#ifndef DAWN_LOGGER_H
+#define DAWN_LOGGER_H
 
-#ifndef Logger_h
-#define Logger_h
-
-#include <jni.h>
 #include <android/log.h>
-#include "obfuscate.h"
 
-enum LogType {
-    oDEBUG = 3,
-    oERROR = 6,
-    oINFO  = 4,
-    oWARN  = 5
+enum daLogType {
+    daDEBUG = 3,
+    daERROR = 6,
+    daINFO = 4,
+    daWARN = 5
 };
 
+//Change this to another Log Tag if ya want. IN the batch script I provide you change the log tag then too
 #define TAG OBFUSCATE("Mod_Menu")
 
-#define LOGD(...) ((void)__android_log_print(oDEBUG, TAG, __VA_ARGS__))
-#define LOGE(...) ((void)__android_log_print(oERROR, TAG, __VA_ARGS__))
-#define LOGI(...) ((void)__android_log_print(oINFO,  TAG, __VA_ARGS__))
-#define LOGW(...) ((void)__android_log_print(oWARN,  TAG, __VA_ARGS__))
+#define LOGD(...) ((void)__android_log_print(daDEBUG, TAG, __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(daERROR, TAG, __VA_ARGS__))
+#define LOGI(...) ((void)__android_log_print(daINFO,  TAG, __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(daWARN,  TAG, __VA_ARGS__))
 
-#endif /* Logger_h */
+#endif //DAWN_LOGGER_H
