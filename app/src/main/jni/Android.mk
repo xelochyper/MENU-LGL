1a8d089a4bae9b1c4f47eca11a4bc04ec1d9fd97
xєꙆo <xelo.chyper@gmail.com>
2026-08-25 22:05:15 +0700

    Update Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 # Here is the name of your lib.
 # When you change the lib name, change also on System.loadLibrary("") under OnCreate method on StaticActivity.java
 # Both must have same name
-LOCAL_MODULE    := MyLibName
+LOCAL_MODULE    := Maino
 
 # Code optimization
 # -std=c++17 is required to support AIDE app with NDK
